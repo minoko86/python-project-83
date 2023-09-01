ce38b225ea7169964f652a041376867eab9b6d4c
Cvilev Andrey <CvilevAS@mail.ru>
2023-09-01 13:30:13 +0500

    add file

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/minoko86/python-project-83/workflows/hexlet-check/badge.svg)](https://github.com/minoko86/python-project-83/actions)
 [![Python CI](https://github.com/minoko86/python-project-83/actions/workflows/pyci.yml/badge.svg)](https://github.com/minoko86/python-project-83/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/3e83d09f9a5f02262901/maintainability)](https://codeclimate.com/github/minoko86/python-project-83/maintainability)
 
 ### Description
 [Analyzer](https://speed-page-analyzer.onrender.com/)
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, get_flashed_messages
 
 
 from dotenv import load_dotenv
@@ -10,5 +10,12 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to Flask!'
+def get_index():
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('base.html', messages=messages)
+
+
+@app.route('/urls')
+def get_index1():
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('urls.html', messages=messages)
