0ca381ec2e46543276eae398fb01622a4a7b0cf6
Cvilev Andrey <CvilevAS@mail.ru>
2023-12-09 18:52:13 +0500

    add files

--- page_analyzer/html.py
+++ page_analyzer/html.py
@@ -3,8 +3,15 @@ from bs4 import BeautifulSoup
 
 def get_seo_data(response):
     soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.title.get_text()
-    h1 = soup.h1.text if soup.h1 else ''
+
+    h1_tag = soup.find('h1')
+    h1 = h1_tag.text if h1_tag else None
+
+    title_tag = soup.title
+    title = title_tag.text if title_tag else None
+
     description_tag = soup.find('meta', attrs={'name': 'description'})
-    description = description_tag.get('content') if description_tag else ''
+    description = description_tag['content'] \
+        if description_tag else None
+
     return h1, title, description
