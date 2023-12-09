b08beb2d9bdb6686ac0cca0a56e67627c510b366
Cvilev Andrey <CvilevAS@mail.ru>
2023-12-09 19:37:45 +0500

    add files

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,18 +1,46 @@
 from datetime import date
 import os
 from psycopg2 import extras
+import psycopg2.pool
 import psycopg2
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
 load_dotenv()
+
+
+def get_db_connection():
+    global db_connect
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    CONF = {
+        'minconn': 1,
+        'maxconn': 20,
+        'cursor_factory': psycopg2.extras.RealDictCursor,
+        'dsn': DATABASE_URL
+    }
+    db_connect = psycopg2.pool.SimpleConnectionPool(**CONF)
+
+
+@contextmanager
+def get_connection():
+    if 'db_connect' not in globals():
+        get_db_connection()
+    try:
+        conn = db_connect.getconn()
+        yield conn
+        conn.commit()
+    except Exception as error:
+        conn.rollback()
+        raise error
+    finally:
+        db_connect.putconn(conn)
 
 
 class DB:
     def get_url_by_id(self, id):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
@@ -27,7 +55,7 @@ class DB:
                 return cursor.fetchone()
 
     def get_checks_by_url_id(self, id):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
@@ -46,7 +74,7 @@ class DB:
                 return cursor.fetchall()
 
     def get_url_by_name(self, name):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
@@ -61,7 +89,7 @@ class DB:
                 return cursor.fetchone()
 
     def get_all_urls(self):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
@@ -76,7 +104,7 @@ class DB:
                 return cursor.fetchall()
 
     def get_last_url_checks(self):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
@@ -97,7 +125,7 @@ class DB:
 
     def create_url(self, name):
         creation_date = date.today()
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             INSERT INTO urls (name, created_at)
@@ -107,7 +135,7 @@ class DB:
                 return cursor.fetchone()[0]
 
     def create_check(self, id, code, h1, title, description):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 creation_date = date.today()
                 cursor.execute("""
@@ -121,7 +149,7 @@ class DB:
                                   creation_date))
 
     def get_check_by_url_id(self, id):
-        with conn:
+        with get_connection() as conn:
             with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cursor:
                 cursor.execute("""
                             SELECT
